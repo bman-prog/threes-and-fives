e4b165636b045a7e03eb079d48854db06ea9575b
bman-prog <bmankovski2028@communityschoolnaples.org>
2025-03-16 14:20:56 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,32 @@
+hero = sprites.create(img("""
+    . . . . . . f f f f . . . . . .
+    . . . . f f f 2 2 f f f . . . .
+    . . . f f f 2 2 2 2 f f f . . .
+    . . f f f e e e e e e f f f . .
+    . . f f e 2 2 2 2 2 2 e e f . .
+    . . f e 2 f f f f f f 2 e f . .
+    . . f f f f e e e e f f f f . .
+    . f f e f b f 4 4 f b f e f f .
+    . f e e 4 1 f d d f 1 4 e e f .
+    . . f e e d d d d d d e e f . .
+    . . . f e e 4 4 4 4 e e f . . .
+    . . e 4 f 2 2 2 2 2 2 f 4 e . .
+    . . 4 d f 2 2 2 2 2 2 f d 4 . .
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . .
+    . . . . . f f f f f f . . . . .
+    . . . . . f f . . f f . . . . .
+"""), SpriteKind.player)
+controller.move_sprite(hero)
+
+number = game.ask_for_number("Enter a number between 1 and 100")
+
+if number % 3 == 0 and number % 5 == 0:
+    hero.say_text("FizzBuzz")
+elif number % 3 == 0:
+    hero.say_text("Fizz")
+elif number % 5 == 0:
+    hero.say_text("Buzz")
+else:
+    hero.say_text(number)
+
+
